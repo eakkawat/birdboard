68bb1bd831a126249a5dc408a35d46788cd9a3d1
eakkawat <eakcomscicmu10@gmail.com>
2020-06-19 12:46:33 +0000

    completed new project mosal can add task

--- app/Project.php
+++ app/Project.php
@@ -8,61 +8,60 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use RecordActivity;
-    
+
     protected $guarded = [];
 
     public static $recordableEvents = ['created', 'updated'];
 
 
-    public function path(){
-        
-        return "/projects/{$this->id}";
+    public function path()
+    {
 
+        return "/projects/{$this->id}";
     }
 
-    public function owner(){
+    public function owner()
+    {
 
         return $this->belongsTo('App\User');
-
     }
 
-    public function tasks(){
+    public function tasks()
+    {
         return $this->hasMany('App\Task');
     }
 
-    public function addTask($body){
+    public function addTask($body)
+    {
 
         return $this->tasks()->create(compact('body'));
-
     }
 
 
-    public function addTasks($tasks){
+    public function addTasks($tasks)
+    {
 
         return $this->tasks()->createMany($tasks);
-
     }
 
 
-    public function activities(){
+    public function activities()
+    {
 
         return $this->hasMany('App\Activity')->latest();
-        
     }
 
 
-    public function invite($user){
+    public function invite($user)
+    {
 
         $this->members()->attach($user);
-        
     }
 
 
-    public function members(){
+    public function members()
+    {
 
         return $this->belongsToMany('App\User', 'project_members')->withTimestamps();
-        
     }
-
-   
 }
